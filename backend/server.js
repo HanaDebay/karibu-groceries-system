ab77f6644edb5e65a67c539d638227a1f6ec35aa
Hana Debay <hana2024ug@gmail.com>
2025-12-25 19:10:10 +0300

    Initialize Node.js project, install dependencies, gitignore and create basic server with MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+//import libraries
+const express = require("express");
+const mongoose = require("mongoose");
+
+//import cors middleware
+const cors = require("cors");
+
+//loads environment variables form .env file
+require("dotenv").config();
+
+//create an express application
+const app = express();
+
+//tells the express app to use CORS middleware
+app.use(cors());
+
+//adds middleware to parse incoming JSON request automatically
+app.use(express.json());
+
+//connect the application to mongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+
+//setup a simple get route for root URI
+app.get("/", (req , res)=>{
+    res.send("Karibu Groceries API running");
+})
+
+//set the port the server will listen on
+const PORT = process.env.PORT || 5000;
+
+
+//start the server and listens for incoming requestes on specified port(5000)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ docs/roles-and-permissions.md
@@ -0,0 +1,15 @@
+Manager:
+- Add/record produce
+- View branch stock
+- Record sales
+- get notified when stock is low or out-of-stock
+
+Sales Agent:
+- Record sales
+- Record credit sales
+- can't add produce
+
+Director:
+- View aggregated reports
+- Manage branches
+- add branhes 
